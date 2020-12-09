6e178131fff322e11607f80a72738bd500509e43
Lakshmi Lavanya <65501898+vllavanya@users.noreply.github.com>
2020-12-09 21:17:57 +0000

    Update exercise003.js
    Logic Added to few problems. Need to work on two more

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,16 +1,42 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
   // Your code here!
+  if (nums.length == 0) {
+    return [];
+  } else {
+    return (Array.from(nums, abc => abc * abc));
+  }
 }
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
   // Your code here!
+
+  var final = "";
+  words.forEach((word, index) => {
+    if (index === 0) {
+      final += word.charAt(0);
+    } else {
+      final += word.charAt(0).toUpperCase()
+    }
+    final += word.slice(1);
+  });
+
+  return final;
+
 }
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+
+  var final = 0;
+  people.forEach(person => {
+    if (person !== undefined || person !== null) {
+      final += person.subjects.length;
+    }
+  });
+
+  return final;
 }
 
 function checkIngredients(menu, ingredient) {
