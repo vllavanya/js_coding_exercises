56018626667e4a3be89cae52d4dd59e0c7a36834
Lakshmi Lavanya <65501898+vllavanya@users.noreply.github.com>
2020-12-09 14:51:45 +0000

    Update exercise002.js
    Logic Added

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,38 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+  return sandwich.fillings
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  return person.city === "Manchester" ? true : false;
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+   return Math.ceil(people / 35)
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  let final = 0
+  arr.forEach(element => {
+    if (element === "Sheep") {
+      final += 1;
+    }
+  });
+  return result
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  let postCode = person.address.postCode
+  // check for existence of letter M in postcode
+  return isNaN(postCode.charAt(1)) ? false : postCode.charAt(0) == "M" ? true : false;
+  
 }
 
 module.exports = {
